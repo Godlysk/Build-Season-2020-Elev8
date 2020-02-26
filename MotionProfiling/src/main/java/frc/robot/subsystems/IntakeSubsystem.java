0d0d622d49bcc712f57742316c204b62f95b1ea1
majorgrizzly <sid@parekh.co.in>
2020-02-26 19:32:04 +0530

    Update IntakeSubsystem.java

--- MotionProfiling/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ MotionProfiling/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -24,7 +24,7 @@ public class IntakeSubsystem extends SubsystemBase {
   }
 
   public void setIntakeSpeed(double speed){
-    intake_master.set(speed);
+    intake_master.set(speed * 0.4);
   }
 
   @Override
